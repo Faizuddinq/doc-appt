d27938a596f7b72325a80d7638890fe3b383da9d
Faizuddin <faizqureshi6126@gmail.com>
2025-05-22 02:15:55 +0530

    updated both to deploy

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ connectDB();
 
 const allowedOrigins= [
     "http://localhost:5173",
+    "http://localhost:3000",
     "https://doc-care-frontend.vercel.app",
     "https://www.doc-care-frontend.vercel.app",
     "http://doc-care-frontend.vercel.app",
