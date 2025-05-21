8ea337e98fee6b8152de5c3e48fefb5533b02571
Faizuddin <faizqureshi6126@gmail.com>
2025-05-22 01:37:24 +0530

    server updated to deploy

--- server/server.js
+++ server/server.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
-const { Server } = require("socket.io");
 require("dotenv").config();
 require("./db/conn");
-require("./controllers/socket");
 const userRouter = require("./routes/userRoutes");
 const doctorRouter = require("./routes/doctorRoutes");
 const appointRouter = require("./routes/appointRoutes");
@@ -13,20 +10,42 @@ const notificationRouter = require("./routes/notificationRouter");
 const app = express();
 const port = process.env.PORT || 5015;
 
-app.use(cors());
+const allowedOrigins= [
+    "http://localhost:5173",
+    "https://doc-care-frontend.vercel.app",
+    "https://www.doc-care-frontend.vercel.app",
+    "http://doc-care-frontend.vercel.app",
+    "http://www.doc-care-frontend.vercel.app",
+  ];
+  
+
+// Middlewares
+app.use(cors({
+    credentials: true,
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    }
+}));
+
 app.use(express.json());
+
+// Root Endpoint
+app.get("/", (req, res) => {
+    res.json({ message: "Hello from POS Server!" });
+});
+
+// Other Endpoints
 app.use("/api/user", userRouter);
 app.use("/api/doctor", doctorRouter);
 app.use("/api/appointment", appointRouter);
 app.use("/api/notification", notificationRouter);
-app.use(express.static(path.join(__dirname, "./client/build")));
 
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-
-const server = app.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
