64d46606ef8bbba8c4db5ef34f152ebabe71bc96
Faizuddin <faizqureshi6126@gmail.com>
2025-05-22 20:08:10 +0530

    updated the UI components of doctor

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,8 @@ import React, { lazy, Suspense } from "react";
 import Loading from "./components/Loading";
 import Dashboard from "./pages/Dashboard";
 
+import "./App.css"
+
 const Aprofile = lazy(()=>import("./components/Aprofile"))
 const Home = lazy(() => import("./pages/Home"));
 const Appointments = lazy(() => import("./pages/Appointments"));
@@ -21,6 +23,9 @@ const DasHome = lazy(() => import("./components/Home"));
 const Notifications = lazy(() => import("./pages/Notifications"));
 const ApplyDoctor = lazy(() => import("./pages/ApplyDoctor"));
 const Error = lazy(() => import("./pages/Error"));
+
+
+
 
 function App() {
   const [userRole, setUserRole] = useState("");
